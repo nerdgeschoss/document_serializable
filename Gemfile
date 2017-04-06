cb4952a624afb3ea48b09ea890c533e7e5a38373
Michael Sievers <msievers@users.noreply.github.com>
2017-04-06 12:50:03 +0200

    Added sane #previous_changes

--- Gemfile
+++ Gemfile
@@ -3,8 +3,9 @@ source 'https://rubygems.org'
 # Specify your gem's dependencies in document_serializable.gemspec
 gemspec
 
-
+gem "activerecord"
 gem "bundler"
 gem "rake"
 gem "rspec", "~> 3.0"
 gem "pry-byebug"
+gem "sqlite3"
--- spec/document_serializable_spec.rb
+++ spec/document_serializable_spec.rb
@@ -58,4 +58,47 @@ describe DocumentSerializable do
     test = Subclass.new slug: "test-slug", id: "5", name: "Testname"
     expect(test.send(:property_object).attributes).to eq id: 5, slug: "test-slug", name: "Testname"
   end
+
+  describe "#previous_changes" do
+    context "if the including class defines #previous_changes" do
+      let(:user_class) do
+        ActiveRecord::Base.establish_connection adapter: "sqlite3", database: ":memory:"
+        ActiveRecord::Schema.define do
+          self.verbose = false
+
+          create_table :users, :force => true do |t|
+            t.string :first_name
+            t.string :last_name
+            t.text :properties
+
+            t.timestamps
+          end
+        end
+
+        Class.new(ActiveRecord::Base) do
+          include DocumentSerializable
+          self.table_name = "users" # because of anonymous class
+
+          serialize :properties, JSON # sqlite has no json type, so we use a text coloumn and serialize on our own
+
+          attribute :some_property, String
+        end
+      end
+
+      let(:updated_user) do
+        user_class.create(first_name: "Max", last_name: "Mustermann", some_property: "bar").tap do |user|
+          user.update_attributes(first_name: "Sabrina", some_property: "muff")
+        end
+      end
+
+      subject { updated_user.previous_changes }
+
+      it { is_expected.to include("first_name"=>["Max", "Sabrina"], "some_property"=>["bar", "muff"]) }
+    end
+
+    context "if the including class does not define #previous_changes" do
+      subject { TestClass.new.previous_changes }
+      it { is_expected.to be_nil }
+    end
+  end
 end
